a32e2ee87747364aa02e2bdfd3d98b5482106db6
crschnick <crschnick@xpipe.io>
2024-06-25 08:24:12 +0000

    10.0-20

--- Casks/Xpipeptb.rb
+++ Casks/Xpipeptb.rb
@@ -1,11 +1,11 @@
 cask "xpipeptb" do
   arch arm: "arm64", intel: "x86_64"
-  version "10.0-19"
+  version "10.0-20"
   desc "XPipe PTB (Public Test Build)"
   homepage "https://xpipe.io"
   url "https://github.com/xpipe-io/xpipe-ptb/releases/download/#{version}/xpipe-installer-macos-#{arch}.pkg",
       verified: "github.com"
-  sha256 arm: "5d1b1d66b958b980e0f3d2a5c8ff5c2cbd36c8a89f1250854d34589d0b177f6e", intel: "6434520707c17f2688afbb04ff237733731c4ec36ea9ca33079acfab83f75da0"
+  sha256 arm: "157c81f98c07e45eb253903fae5b39951d101ae2ebc7b7543130bc3de4e42858", intel: "f31a172a60d0dacea1a725999d57c7cde2b46853eb4a1181eb496c60383fed9f"
   name "XPipe PTB"
   auto_updates true
 
