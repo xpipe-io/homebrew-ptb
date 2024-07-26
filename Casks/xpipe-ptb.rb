8ce94405206f6422b498296e676b024318150dbe
crschnick <crschnick@xpipe.io>
2024-07-26 14:42:47 +0000

    10.2.2-1

---
+++ Casks/xpipe-ptb.rb
@@ -0,0 +1,30 @@
+cask "xpipe-ptb" do
+  arch arm: "arm64", intel: "x86_64"
+  version "10.2.2-1"
+  desc "XPipe PTB (Public Test Build)"
+  homepage "https://xpipe.io"
+  url "https://github.com/xpipe-io/xpipe-ptb/releases/download/#{version}/xpipe-installer-macos-#{arch}.pkg",
+      verified: "github.com"
+  sha256 arm: "c11ba89ba1babe25684f038ab127ac1c142a85dade894d13c8542dd581021987", intel: "509acd4070719fbf610035598530c91f75d21e457ba89d75b4782c9b2e36c78b"
+  name "XPipe PTB"
+  auto_updates true
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on formula: "util-linux"
+  depends_on formula: "netcat"
+
+  pkg "xpipe-installer-macos-#{arch}.pkg"
+  uninstall script:  {
+        executable: "/Applications/XPipe PTB.app/Contents/Resources/scripts/uninstall.sh",
+        args:       [],
+        sudo:       true,
+      },
+      pkgutil: "io.xpipe.xpipe-ptb"
+  zap trash: [
+    "~/.xpipe-ptb"
+  ]
+end
